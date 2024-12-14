8ca3d2acc48339765472cc4b987b43a01462e273
U.P.S.D.DULAJ <e20456@eng.pdn.ac.lk>
2024-12-14 22:38:55 +0530

    Initialize Spring Boot web application project
    Set up a basic Spring Boot web application with Maven. Added the main application class, build configuration (pom.xml), and essential dependencies. Introduced Maven wrapper, .gitignore, and application properties to enable smooth development and database configuration.

---
+++ src/main/java/org/springboot_backend/simplespringbootwebapplication/SimpleSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package org.springboot_backend.simplespringbootwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springboot_backend/simplespringbootwebapplication/SimpleSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot_backend.simplespringbootwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
